4cdd4c37efd2d08484ceaf0cc44a734ee611b781
Aequin0x <thibaut.desrame@gmail.com>
2016-07-07 14:47:05 +0200

    add voir ses tweets & mise en forme de ceux-ci

--- twitter.php
+++ twitter.php
@@ -11,20 +11,48 @@ $access_token_secret = 'GIG7k198NY4KFoHlbVfEHZ7UDTlogxPpdZ6qwdtsm0CHK';
 $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token, $access_token_secret);
 $content = $connection->get("account/verify_credentials");
 
-echo $content->name;
+// echo $content->name;
 
 
-// Pour ecrire un tweet
+
+// Pour ecrire un tweet //
+
 // $connection->post("statuses/update", array("status" => "https://horsjeu.football/ "));
 
-// Pour suivre quelqu'un
+
+// Pour suivre quelqu'un //
+
 // $connection->post("friendships/create", array("screen_name" => "Ass059"));
 
-// Pour lister des users par un mot clef
+
+// Pour lister des users par un mot clef //
+
 $suggestions = $connection->get('users/search', array('q' => 'web', 'count' => 5));
-// var_dump($suggestions);
+// Pour les suivres //
 foreach ($suggestions as $suggestion) {
 	echo $suggestion->screen_name;
 	$connection->post('friendships/create', array('screen_name' => $suggestion->screen_name));
 }
+
+// Tweeter une image via lorempixel
+
+// $media1 = $connection->upload('media/upload', ['media' => 'http://qnimate.com/wp-content/uploads/2014/03/images2.jpg']); // Your img here
+// $parameters = [
+// 	'status' => 'img',
+// 	'media_ids' => implode(',', [$media1->media_id_string])
+// ];
+// $result = $connection->post('statuses/update', $parameters);
+
+
+// Pour voir tout ses tweets //
+
+$tweets = $connection->get('statuses/user_timeline', array('user_id' => $content->id));
+//var_dump($tweets);
+
+// Pour mettre en forme ses tweets grace // 
+foreach ($tweets as $tweet) {
+	$embedTweet = $connection->get('statuses/oembed', array('url' => 'https://twitter.com/'.$content->screen_name.'/status/'.$tweet->id));
+	echo $embedTweet->html;
+}
+
 ?>
